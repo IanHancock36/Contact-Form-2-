8ae683aa37110ce9eca5ddce284134871742426a
Ian Hancock <northroadian@gmail.com>
2021-02-06 12:24:54 -0600

    first commit

---
+++ form.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('name')
+const businessName = document.getElementById('businessname')
+const emailName = document.getElementById('emailaddress')
+const phoneNumber = document.getElementById('phonenumber')
+
+// need to do an event listener to 
+
