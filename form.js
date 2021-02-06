af2a1059931d370faff2a1f1569900237729c8e7
Ian Hancock <northroadian@gmail.com>
2021-02-06 12:57:25 -0600

    added background and notes of further checklist

--- form.js
+++ form.js
@@ -3,5 +3,8 @@ const businessName = document.getElementById('businessname')
 const emailName = document.getElementById('emailaddress')
 const phoneNumber = document.getElementById('phonenumber')
 
-// need to do an event listener to 
+// need to do an event listener to listen to the individual 
+// id's to check the keys match with what you want and 
+// if not return error messages. 
+// 
 
